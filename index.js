a8a202e6accaa2ea895eb171d2ff2c2c30246955
WAHID ALI <ali76wahid@gmail.com>
2024-12-06 11:13:03 +0500

    Problem-1 longest-uncommon-subsequence

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Starting from 6 December 2024 Daily 1 Problem ==Streak 1
 
 //Problem-1 longest-uncommon-subsequence
+//Catogery:String
 var findLUSlength = function(a, b) {
     if(a===b){
         return -1
