aba911024cc25463730ebe18e755df635be3acfe
WAHID ALI <ali76wahid@gmail.com>
2024-12-06 11:05:10 +0500

    Day 1 Problem#1

---
+++ index.js
@@ -0,0 +1,11 @@
+//Starting from 6 December 2024 Daily 1 Problem ==Streak 1
+
+//Problem-1 longest-uncommon-subsequence
+var findLUSlength = function(a, b) {
+    if(a===b){
+        return -1
+    }
+    else{
+         return Math.max(a.length, b.length);
+    }
+};
